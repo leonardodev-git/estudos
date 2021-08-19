28a1582fcec36eddb50670bd53330d4b6fb32fc6
Leonardo <leo.maga17@gmail.com>
2021-08-18 21:01:52 -0300

    Finish fisrt module

--- resolvers/Produto.js
+++ resolvers/Produto.js
@@ -1,11 +1,9 @@
-
 module.exports = {
-    precoComDesconto(produto) {
-        if(produto.desconto) {
-            return produto.preco 
-                * (1 - produto.desconto)
-        } else {
-            return produto.preco
-        }
+  precoComDesconto(produto) {
+    if (produto.desconto) {
+      return produto.preco * (1 - produto.desconto);
+    } else {
+      return produto.preco;
     }
-}
+  },
+};
